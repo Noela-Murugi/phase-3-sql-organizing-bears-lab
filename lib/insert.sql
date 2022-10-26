32d17512441db6b1cf1794a3c161603fade9f767
Noela-Murugi <murugin254@gmail.com>
2022-10-26 16:17:13 +0300

    completed the assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Mr. Chocolate", 50 ,"M", "Red" , "Grumpy" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Rowdy", 69 ,"F", "Orange" , "Happy" , false);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Tabitha", 21 ,"F", "Black" , "Grumpy" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Sergeant Brown", 36 ,"M", "Grey" , "Grumpy" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Melissa", 44 ,"F", "Blue" , "Clumsy" , false);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Grinch", 11 ,"M", "Yellow" , "Sad" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Wendy", 12 ,"F", "White" , "Happy" , false);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ( NULL,13,"M", "Pink" , "Melencholic" , true);
